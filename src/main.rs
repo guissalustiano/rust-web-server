9db7627f8c8fd5cfa50934c2c2389143976855bf
Guilherme Salustiano <guissalustiano@gmail.com>
2021-01-16 18:47:14 -0300

    Single-Threaded implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::thread;
+use std::time::Duration;
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let (status_line, filename) = if buffer.starts_with(b"GET / ") {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(b"GET /sleep ") {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\n{}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
