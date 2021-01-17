b5aa332df0f7100463115fe4761556b1bdf2cb34
Guilherme Salustiano <guissalustiano@gmail.com>
2021-01-16 22:07:26 -0300

    Mult-Thread Implementation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,14 +3,18 @@ use std::time::Duration;
 use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use my_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
